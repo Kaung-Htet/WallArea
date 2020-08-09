e477629470cbc57dca68d04378f36ffd4d8abe05
Kaung-Htet <kaunghtetnyilay@gmail.com>
2020-08-09 22:28:56 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+
+//→ TEST CODE:
+//
+//1 Wall wall = new Wall(5,4);
+//2 System.out.println("area= " + wall.getArea());
+//3
+//4 wall.setHeight(-1.5);
+//5 System.out.println("width= " + wall.getWidth());
+//6 System.out.println("height= " + wall.getHeight());
+//7 System.out.println("area= " + wall.getArea());
+//
+//→ OUTPUT:
+//
+//area= 20.0
+//width= 5.0
+//height= 0.0
+//area= 0.0
